b9a9112eed90c474ef39ce930877af186b9636e6
chineduprincewill <chinedu.ijeomah@yahoo.com>
2020-04-18 16:43:01 +0100

    added state persistence function to fix state loss on page reload

--- ecommerce-project/src/store.js
+++ ecommerce-project/src/store.js
@@ -4,14 +4,40 @@ import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
-const initialState = {};
+function saveToLocalStorage(state){
+    try{
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('state', serializedState);
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+
+function loadFromLocalStorage(){
+    try{
+        const serializedState = localStorage.getItem('state');
+        if(serializedState === null) return undefined;
+        return JSON.parse(serializedState);
+    }
+    catch(e){
+        console.log(e)
+        return undefined
+    }
+}
+
+//const initialState = {};
 
 const middleware = [thunk];
 
+const persistedState = loadFromLocalStorage();
+
 const store = createStore(
     rootReducer,
-    initialState,
+    persistedState,
     composeWithDevTools(applyMiddleware(...middleware))
 );
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
 
 export default store;
